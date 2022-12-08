dd6a8e6944f661a4e3de4a3cb428c8a30555f4e8
Farhanham22pv192 <89659467+Farhanham22pv192@users.noreply.github.com>
2022-12-09 02:00:34 +0300

    Delete src/main/java/tech/reliab/course/Farhanham directory

--- src/main/java/tech/reliab/course/Farhanham/service/BankService.java
+++
@@ -1,9 +0,0 @@
-package tech.reliab.course.farhanham.bank.service;
-import tech.reliab.course.farhanham.bank.entity.Bank;
-
-public interface BankService {
-    Bank create(String name);
-    Bank read();
-    void update(Bank bank);
-    void delete(Bank bank);
-}
